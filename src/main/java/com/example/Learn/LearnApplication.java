378247ba476bd6c96c8bfa7af86dc74aa813b52c
shyamsah23 <92447257+shyamsah23@users.noreply.github.com>
2025-02-15 15:55:09 +0530

    second branch commit

--- src/main/java/com/example/Learn/LearnApplication.java
+++ src/main/java/com/example/Learn/LearnApplication.java
@@ -9,6 +9,7 @@ public class LearnApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearnApplication.class, args);
 		System.out.println("Message from first Branch");
+		System.out.println("Message from seond Branch");
 	}
 
 }
